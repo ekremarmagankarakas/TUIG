cdbcb6ca722e24275b7704de7c15037e719ca8ba
ekremarmagankarakas <ekremarmagankarakas@gmail.com>
2026-06-13 19:33:33 -0400

    Add Homebrew formula

---
+++ Formula/tuig.rb
@@ -0,0 +1,32 @@
+class Tuig < Formula
+  desc "Terminal UI Games launcher with chess and Turkish draughts"
+  homepage "https://github.com/ekremarmagankarakas/TUIG"
+  url "https://github.com/ekremarmagankarakas/TUIG.git",
+      using:    :git,
+      tag:      "v0.1.0",
+      revision: "8a91c8ba31513c4be28bc07bf2fc8a8741189bc9"
+  license "MIT"
+  head "https://github.com/ekremarmagankarakas/TUIG.git", branch: "main"
+
+  depends_on "cmake" => :build
+
+  def install
+    # chesscli and damacli are git submodules; fetch their contents before
+    # configuring CMake (GitHub source tarballs would omit them).
+    system "git", "submodule", "update", "--init", "--recursive"
+
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DCMAKE_BUILD_TYPE=Release",
+                    *std_cmake_args
+    system "cmake", "--build", "build", "--parallel"
+
+    bin.install "build/tuig",
+                "build/chesscli/chesscli",
+                "build/damacli/damacli"
+  end
+
+  test do
+    system bin/"chesscli", "--help"
+    system bin/"damacli",  "--help"
+  end
+end
